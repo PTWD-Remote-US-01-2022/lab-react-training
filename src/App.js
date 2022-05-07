4a95b64fba1a443fd61e5d5aaf82f3d64f205703
rabiulalam1 <rabiul.alam@outlook.com>
2022-05-07 15:32:41 -0400

    6-11

--- src/App.js
+++ src/App.js
@@ -76,7 +76,7 @@ function App() {
       <Rating>0</Rating>
       <Rating>1.49</Rating>
       <Rating>1.5</Rating>
-      <Rating>3</Rating>
+      <Rating>3.5</Rating>
       <Rating>4</Rating>
       <Rating>5</Rating>
 
